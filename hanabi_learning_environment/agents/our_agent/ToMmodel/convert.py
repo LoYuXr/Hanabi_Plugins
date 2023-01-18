8d1b9279ff57413f312ad25d0f8cc7624ecccde8
Qian Yilue <fengzhaqiQYQC@outlook.com>
2023-01-18 09:11:13 +0000

    add tom3-4 onnx files

--- hanabi_learning_environment/agents/our_agent/ToMmodel/convert.py
+++ hanabi_learning_environment/agents/our_agent/ToMmodel/convert.py
@@ -7,7 +7,7 @@ config.num_players = 3
 
 dataset = ToMDataset('/home/yilue/datasets/files', max_data_num=1, config=config)
 model = ToMModel(dataconfig=dataset.config)
-model.load_state_dict(torch.load('tom_ckpt/tom_16.pth'))
+model.load_state_dict(torch.load('tom_ckpt3/tom_16.pth'))
 
 d_input = dataset[0][0]
 d_input_1, d_input_2 = d_input
@@ -17,5 +17,5 @@ d_input = [d_input_1, d_input_2]
 # d_output = model(d_input)
 # d_output = model((d_input_1,d_input_2))
 
-torch.onnx.export(model.cuda(), d_input, 'tom4.onnx', verbose=True,
+torch.onnx.export(model.cuda(), d_input, 'tom3.onnx', verbose=True,
                   input_names=['act_seq', 'obs'], output_names=['output'])
